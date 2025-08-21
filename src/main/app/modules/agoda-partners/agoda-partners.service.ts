7b285bb3fb97e8d244c7ff2e69efdcf8cee01899
efficier <k37844350@gmail.com>
2025-08-21 15:40:51 +0900

    파트너스 API 방식수정

--- src/main/app/modules/agoda-partners/agoda-partners.service.ts
+++ src/main/app/modules/agoda-partners/agoda-partners.service.ts
@@ -8,20 +8,22 @@ export class AgodaPartnersService {
 
   constructor(private readonly settingsService: SettingsService) {}
 
-  // 간단 어필리에이트 링크: 원본 URL에 파라미터 부착만 수행
+  // 어필리에이트 링크: 파트너 검색 경유 링크 생성 (partnersearch.aspx?cid=...&url=...)
   async createAffiliateLink(originalUrl: string): Promise<AgodaAffiliateLink> {
     const settings = await this.settingsService.getSettings()
     // 우선순위: 설정의 apiKey에서 CID 파싱
     const rawApiKey = settings?.agoda?.apiKey || ''
     const [partnerId] = rawApiKey.split(':')
 
-    // 기본: ?cid=partnerId 형태. 이미 쿼리가 있으면 &cid=
-    const url = new URL(originalUrl)
-    if (!url.searchParams.has('cid') && partnerId) {
-      url.searchParams.set('cid', partnerId)
-    }
-
-    const finalUrl = url.toString()
+    // 파트너 ID가 있으면 아고다 파트너 경유 링크로 변환, 없으면 원본 URL 유지
+    const finalUrl = partnerId
+      ? (() => {
+          const partnerUrl = new URL('https://www.agoda.com/partners/partnersearch.aspx')
+          partnerUrl.searchParams.set('cid', partnerId)
+          partnerUrl.searchParams.set('url', originalUrl)
+          return partnerUrl.toString()
+        })()
+      : originalUrl
     return {
       originalUrl,
       shortenUrl: finalUrl,
