bd6b00c4bd1a633cd50ffae3f2fbbe0c8e8fced8
efficier <k37844350@gmail.com>
2025-09-21 13:33:42 +0900

    lint

--- src/main/app/modules/workflow/dto/coupang-blog-post-workflow.dto.ts
+++ src/main/app/modules/workflow/dto/coupang-blog-post-workflow.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsNotEmpty, IsOptional, IsUrl, IsBoolean, IsNumber, Min, Max } from 'class-validator'
+import { IsString, IsNotEmpty, IsOptional, IsBoolean, IsNumber, Min, Max } from 'class-validator'
 import { Transform, Type } from 'class-transformer'
 
 /**
